e309bff64368f60836d48c70fed2941ca1210502
Harshini <163083363+harshini-224@users.noreply.github.com>
2025-12-20 10:12:45 +0530

    Create twilio_calls.py

---
+++ backend/twilio_calls.py
@@ -0,0 +1,14 @@
+import os
+from twilio.rest import Client
+
+client = Client(
+    os.environ["TWILIO_SID"],
+    os.environ["TWILIO_AUTH"]
+)
+
+def call_patient(phone: str):
+    return client.calls.create(
+        to=phone,
+        from_=os.environ["TWILIO_NUMBER"],
+        url=f"{os.environ['BACKEND_URL']}/twilio/voice"
+    ).sid
